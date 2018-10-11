7ea417810a9c3b87b3058b2fc85f27f7e021c6d3
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-11 12:59:28 -0500

    Add navbar

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<events-list></events-list>',
+  template: '<navbar></navbar><events-list></events-list>',
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
