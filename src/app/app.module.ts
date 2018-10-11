8ac792d0e5ef688db29069c46a24b907c685df69
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-11 16:35:42 -0500

    Add event service and toaster service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
+import {EventService} from './events/shared/event.service';
+import {ClientNotificationService} from './common/client-notification.service';
 
 @NgModule({
   imports: [
     BrowserModule
+  ],
+  providers:[
+    EventService,
+    ClientNotificationService
   ],
   declarations: [
     EventsAppComponent,
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,9 +5,20 @@ import {Component, Input } from '@angular/core'
     templateUrl: 'event-thumbnail.component.html',
     styles:[
         ".pad-left{margin-left:10px;}",
-        ".well div {color: #bbb}"
+        ".well div {color: #bbb}",
+        ".thumbnail{min-height:210px;}",
+        ".green{color:green !important;}",
+        ".red{color:red !important;}",
+        ".bold, .strong{font-weight:bold !important;}"
     ]
 })
 export class EventThumbnailComponent{
     @Input() event: any;
+
+
+    getStartTimeClass(){
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green:isEarlyStart, bold:isEarlyStart, strong:!isEarlyStart};
+    }
+    
 }
