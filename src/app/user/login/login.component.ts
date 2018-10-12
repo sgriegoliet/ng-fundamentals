2c1a1e1ae4e1a683e5c89e095433a4d33fe73b46
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-12 12:38:52 -0500

    Add login form validation

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,16 +3,18 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: ["em {float:right; color:#E05C65; padding-left:10px;}"]
 })
 export class LoginComponent {
     username: string;
     password: string;
+    mouseoverLogin:boolean;
 
     constructor(private authService: AuthService, private router: Router) {
     }
 
-    cancel(){
+    cancel() {
         this.router.navigate(['events']);
     }
 
