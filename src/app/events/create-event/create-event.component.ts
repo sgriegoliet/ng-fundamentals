f5435588234cd913f0c4820dce8456ddba83702a
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-12 18:27:57 -0500

    Basic validation for session form

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -5,18 +5,9 @@ import { IEvent, EventService } from '../shared';
 
 @Component({
     templateUrl: 'create-event.component.html',
-    styles: [
-        'em {float:right; color:#E05C65; padding-left:10px;}',
-        '.error input {background-color: #E3C3C5;}',
-        '.error ::-webkit-input-placeholder {color:#999;}',
-        '.error ::-moz-placeholder {color:#999;}',
-        '.error :-moz-placeholder {color:#999;}',
-        '.error :ms-input-placeholder {color:#999;}',
-    ]
 })
 export class CreateEventComponent implements IDirtyableComponent {
     constructor(private router: Router, private eventService: EventService) {
-
     }
 
     isCreateFormDirty:boolean = true;
--- src/app/events/event-details/create-session/create-session.component.ts
+++ src/app/events/event-details/create-session/create-session.component.ts
@@ -1,8 +1,53 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { ISession } from '../../shared';
 
 @Component({
-    templateUrl:'create-session.component.html'
+    templateUrl: 'create-session.component.html'
 })
-export class CreateSessionComponent{
+export class CreateSessionComponent implements OnInit {
+    name: FormControl;
+    presenter: FormControl;
+    duration: FormControl;
+    level: FormControl;
+    abstract: FormControl;
+    newSessionForm: FormGroup;
 
+    ngOnInit() {
+        this.name = new FormControl('', Validators.required);
+        this.presenter = new FormControl('', Validators.required);
+        this.duration = new FormControl('', Validators.required);
+        this.level = new FormControl('', Validators.required);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+        this.newSessionForm = new FormGroup({
+            name: this.name,
+            presenter: this.presenter,
+            duration: this.duration,
+            level: this.level,
+            abstract: this.abstract,
+        })
+    }
+
+    validate(field: string, validator: string) {
+        if (validator == undefined) {
+            return !(this[field].invalid && this[field].dirty);
+        }
+        else {
+            return !(this[field].invalid && this[field].dirty && this[field].errors[validator]);
+        }
+    }
+
+    saveSession(formValues) {
+        let session: ISession = {
+            id: undefined,
+            name: formValues.name,
+            duration: +formValues.duration,
+            level: formValues.level,
+            presenter: formValues.presenter,
+            abstract: formValues.abstract,
+            voters: [],
+        }
+        console.log(session);
+    }
 }
