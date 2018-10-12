f1cd5c26e7b194422014a368e1b29ccc6a2ef1d8
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-12 13:31:38 -0500

    Add some validation to profile form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,31 +1,47 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [
+    'em {float:right; color:#E05C65; padding-left:10px;}',
+    '.error input {background-color: #E3C3C5;}',
+    '.error ::-webkit-input-placeholder {color:#999;}',
+    '.error ::-moz-placeholder {color:#999;}',
+    '.error :-moz-placeholder {color:#999;}',
+    '.error :ms-input-placeholder {color:#999;}',
+  ]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  private firstName:FormControl;
+  private lastName:FormControl;
 
   constructor(private authService: AuthService, private router: Router) {
 
   }
 
   ngOnInit() {
-    var currentUser = this.authService.currentUser;
-    let firstName = new FormControl(currentUser.firstName);
-    let lastName = new FormControl(currentUser.lastName);
+    let currentUser = this.authService.currentUser;
+    this.firstName = new FormControl(currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
+    this.lastName = new FormControl(currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
-  saveProfile(formValues){
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
+  saveProfile(formValues) {
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
+  }
+
+  validate(field: string): boolean {
+    return this[field].valid || this[field].untouched;
   }
 
   cancel() {
