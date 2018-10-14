4080f8404f1b29849c8b908930a41afa09664ada
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-14 18:33:16 -0500

    use input setter to set style of vote button

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -1,15 +1,19 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 @Component({
-    selector:'upvote',
-    templateUrl:'upvote.component.html',
-    styleUrls:['upvote.component.css']
+    selector: 'upvote',
+    templateUrl: 'upvote.component.html',
+    styleUrls: ['upvote.component.css']
 })
-export class UpvoteComponent{
-    @Input() count:number;
-    @Input() voted:boolean;
+export class UpvoteComponent {
+    @Input() count: number;
+    @Input() set voted(val) {
+        this.iconColor = val ? 'red' : 'white';
+    };
     @Output() vote = new EventEmitter();
 
-    onClick(){
+    iconColor: string;
+
+    onClick() {
         this.vote.emit({});
     }
 }
