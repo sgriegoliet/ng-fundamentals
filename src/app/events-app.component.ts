125f12cecfc9ee9dc0349b104e890c918171afc3
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-14 22:12:29 -0500

    get current user on reload

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './user';
 
 @Component({
   selector: 'events-app',
   template: `<navbar></navbar>
   <router-outlet></router-outlet>`,
 })
-export class EventsAppComponent {
-  title = 'ng-fundamentals';
+export class EventsAppComponent implements OnInit {
+  constructor(private auth: AuthService) {
+
+  }
+
+  ngOnInit() {
+    this.auth.checkAuthenticationStatus();
+  }
 }
--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -34,4 +34,14 @@ export class AuthService {
     isAuthenticated() {
         return !!this.currentUser;
     }
+
+    checkAuthenticationStatus(){
+        this.http.get('/api/currentIdentity')
+        .pipe(tap(data=>{
+            if (data instanceof Object){
+                this.currentUser = <IUser>data;
+            }
+        }))
+        .subscribe();
+    }
 }
