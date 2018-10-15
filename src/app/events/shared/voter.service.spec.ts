3d1cc6349e8fc30f31cecc5f4883c7d3dd4e96f9
Steve Griegoliet <steve.griegoliet@slalom.com>
2018-10-15 10:10:57 -0500

    More unit tests on voter service

--- src/app/events/shared/voter.service.spec.ts
+++ src/app/events/shared/voter.service.spec.ts
@@ -2,6 +2,7 @@ import { VoterService } from './voter.service'
 import { ISession } from './event.model';
 import { IUser } from 'src/app/user';
 import { of } from 'rxjs';
+import { exec } from 'child_process';
 
 
 describe('VoterService', () => {
@@ -23,5 +24,39 @@ describe('VoterService', () => {
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe("john");
         })
+
+        it('should call http.delete with the right URL', ()=>{
+            var session = { id: 6, voters: ["joe", "john"] };
+            var user = {userName:"joe"};
+
+            mockHttp.delete.and.returnValue(of(false));
+            voterService.deleteVoter(3, <ISession>session, <IUser>user);
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe')
+        });
+    });
+
+    describe('addVoter', () => {
+        it('should add the voter form the list of voters', () => {
+            var session = { id: 1, voters: ["john"] };
+            var user = {userName:"joe"};
+
+            mockHttp.post.and.returnValue(of(false));
+            voterService.addVoter(3, <ISession>session, <IUser>user);
+
+            expect(session.voters.length).toBe(2);
+            expect(session.voters).toContain("joe");
+        })
+
+
+        it('should call http.post with the right URL and data', ()=>{
+            var session = { id: 6, voters: ["john"] };
+            var user = {userName:"joe"};
+
+            mockHttp.post.and.returnValue(of(false));
+            voterService.addVoter(3, <ISession>session, <IUser>user);
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe',{}, jasmine.any(Object));
+        });
     });
 })
